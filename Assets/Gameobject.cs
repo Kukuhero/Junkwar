0efc8990895cbba07b1ea6bf1fa8fa48f9bf94b0
apcnc <fchluca@web.de>
2016-10-21 20:32:09 +0200

    xml erweiterung
    teilweises laden eines neuen gameobjects durch xml

--- Assets/Gameobject.cs
+++ Assets/Gameobject.cs
@@ -9,4 +9,23 @@ public class Gameobject  {
 	public int damage;
 	[XmlElement("health")]
     public int Health;
+	[XmlElement("splash")]
+	public int splash;
+	[XmlElement("collidersizex")]
+	public int collidersizex;
+	[XmlElement("collidersizey")]
+	public int collidersizey;
+	[XmlElement("collidersizez")]
+	public int collidersizez;
+	[XmlElement("colliderposx")]
+	public int colliderposx;
+	[XmlElement("colliderposy")]
+	public int colliderposy;
+	[XmlElement("colliderposz")]
+	public int colliderposz;
+	[XmlElement("loot")]
+	public int loot;
+	[XmlElement("speed")]
+	public int speed;
+
 }
--- Assets/LoadGameObject.cs
+++ Assets/LoadGameObject.cs
@@ -6,15 +6,29 @@ using System.Xml.Serialization;
 using System.IO;
 
 public class LoadGameObject : MonoBehaviour {
-
+	int i = 0;
 	public const string path = "GameObjectstats";
 	// Use this for initialization
 	void Start () {
 
 		GameObjectContainer go = GameObjectContainer.Load (path);
 		print (go.Gameobjects.Count);
+		GameObject[] Gameobjects = new GameObject[go.Gameobjects.Count];
 		foreach (Gameobject Gameobject in go.Gameobjects) {
 			print (Gameobject.Name);
+			Gameobjects [i].name = Gameobject.Name;
+			Gameobjects [i].AddComponent<HeldController> ();
+			Gameobjects [i].GetComponent<HeldController> ().speed = Gameobject.speed;
+			Gameobjects [i].AddComponent<Health> ();
+			Gameobjects [i].GetComponent<Health> ().health = Gameobject.Health;
+			Gameobjects [i].AddComponent<Animator> ();
+			Gameobjects [i].AddComponent<BoxCollider> ();
+			Gameobjects [i].GetComponent<BoxCollider> ().size.x = Gameobject.collidersizex;
+			Gameobjects [i].GetComponent<BoxCollider> ().size.y = Gameobject.collidersizey;
+			Gameobjects [i].GetComponent<BoxCollider> ().size.z = Gameobject.collidersizez;
+			Gameobjects [i].GetComponent<BoxCollider> ().center.x = Gameobject.colliderposx;
+			Gameobjects [i].GetComponent<BoxCollider> ().center.y = Gameobject.colliderposy;
+			Gameobjects [i].GetComponent<BoxCollider> ().center.z = Gameobject.colliderposz;
 		}
 
 	}
